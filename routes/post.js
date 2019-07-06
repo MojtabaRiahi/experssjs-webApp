011f45eeafe7f15601aa34ea7217abfdbffaf6c3
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-06 18:53:17 +0430

    bug fixed

--- routes/post.js
+++ routes/post.js
@@ -1,49 +1,38 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const router = express.Router()
-const { Post, validate } = require('../models/post')
-const lodash = require('lodash')
+const { validate } = require('../models/post')
+const PostService = require('../service/post.service')
 const checkRole = require('../middleware/checkRole')
 const checkAuth = require('../middleware/checkAuth')
 const asyncMiddleware = require('../middleware/asyncmiddleware')
 
+const postService = new PostService();
+
 router.post('/create', [checkAuth, checkRole], asyncMiddleware(async (req, res) => {
     const { error } = await validate(req.body);
     if (error) { return res.status(400).send(error.details[0].message) };
-    const post = new Post(lodash.pick(req.body, ['title', 'description', 'tags', 'imageUrl']))
-    post.author = req.user._id;
-    await post.save();
+    //postService = new postService();
+    const post = await postService.createPost(req.body, req.user._id)
     return res.status(200).send(`new post created:${post}`)
 }))
+
 //update post
 router.put('/edit/:id', [checkAuth, checkRole], asyncMiddleware(async (req, res) => {
     const { error } = await validate(req.body);
     if (error) { return res.status(400).send(error.details[0].message) };
-    let findPost = await Post.findById(req.params.id);
-    if (!findPost) return res.status(400).send(`there is not post by this ${req.params.id}`)
-
-    const post = new Post(lodash.pick(req.body, ['title', 'description', 'tags', 'imageUrl']))
-    post.author = req.user._id;
-
-    findPost.title = post.title;
-    findPost.description = post.description;
-    findPost.imageUrl = post.imageUrl;
-    findPost.author = post.author;
-    findPost.tags = post.tags;
-    await findPost.save();
-    return res.status(200).send(`new post created:${findPost}`)
+    const result = await postService.updatePost(req.params.id, req.body, req.user._id);
+    if (!result.title) return res.status(400).send(`bad-request : ${result}`)
+    return res.status(200).send(` post updated:${result}`)
 }))
 //delete post 
 router.delete('/delete/:id', [checkAuth, checkRole], asyncMiddleware(async (req, res) => {
-
-    let findPost = await Post.findById(req.params.id);
-    if (!findPost) return res.status(400).send(`there is not post by this ${req.params.id}`)
-    await Post.deleteOne(findPost)
+    const result = await postService.deletePost(req.params.id);
+    if (!result.ok) return res.status(400).send(`bad-request : ${result}`)
     return res.status(200).send('post is deleted')
 }))
 //get all post
 router.get('/', [checkAuth, checkRole], asyncMiddleware(async (req, res) => {
-    const posts = await Post.find();
+    const posts = await postService.getAllPost()
     return res.status(200).send(`all post : ${posts}`)
 }))
 module.exports = router
--- service/post.service.js
+++ service/post.service.js
@@ -1,12 +1,28 @@
-const { Post} = require('../models/post')
+const { Post } = require('../models/post')
 const lodash = require('lodash')
-
-
 class PostService {
 
-    async createpost(newPost,authorId){
+    async createPost(newPost, authorId) {
         const post = new Post(lodash.pick(newPost, ['title', 'description', 'tags', 'imageUrl']))
         post.author = authorId;
-        await post.save();
+       return  await post.save();
+    }
+    async updatePost(postId, newPostData, author) {
+        const { title, description, imageUrl, tags } = newPostData;
+        let post = await this.findPostById(postId)
+        if (!post) return new Error(`there is not post by this id: ${postId}`);
+        return  await post.set({ title, description, imageUrl, tags, author }).save();
+    }
+    async deletePost(postId) {
+        const post = await this.findPostById(postId)
+        if (!post) return await new Error(`there is not post by this id: ${postId}`)
+        return await Post.remove(post)
+    }
+    async getAllPost() {
+        return await Post.find();
+    }
+    async findPostById(postId) {
+        return  await Post.findById(postId);
     }
 }
+module.exports = PostService;
