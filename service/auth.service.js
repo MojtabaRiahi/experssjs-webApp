027ddef66e182d4a6444018cdb03c5fde4df3345
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-08 13:06:33 +0430

    create login method

--- service/auth.service.js
+++ service/auth.service.js
@@ -1,20 +1,40 @@
 const UserSerVice = require('./user.service')
-
+const lodash = require('lodash')
 class AuthService {
 
     constructor() {
-      userService=new UserSerVice();
+        userService = new UserSerVice();
     }
     async register(userData) {
         const result = userService.createUser(userData)
-        if(result.ok){
-            await this.userLogin(result)
-        }
+        if (!result.ok) return result;
+
+        await this.userLogin(result)
+
 
     }
-    async userLogin(user){
+    async userLogin(userData) {
+        //if req is validate => find user
+        let user = await User.findOne({ email: userData.email });
+        if (!user) return new Error('invalid username or password !')
+        //check password
+        const checkPass = await this.checkPassword(userData.password, user.password)
+        if (!checkPass) return new Error('invalid username or password !')
         const token = user.generateAuthToken();
-       // res.header('x-auth-token', token).send(lodash.pick(user, ["name", "email"]));
+        const data = { token: token, user: lodash.pick(user, ['name', 'email']) };
+        return data;
+    }
+    async checkPassword(sendPassword, userPassword) {
+        return await bcrypt.compare(sendPassword, userPassword);
+
+    }
+    //validate login data
+    validate(loginData) {
+        const schema = {
+            email: joi.string().min(5).max(255).required().email(),
+            password: joi.string().min(5).max(1024).required()
+        }
+        return joi.validate(loginData, schema)
     }
 }
 module.exports = AuthService;
