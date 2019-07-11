46e3f51a866d7909a68f76d20037421711184575
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-11 12:02:36 +0430

    create loggin module in startup

---
+++ startup/loggin.js
@@ -0,0 +1,37 @@
+//require('express-async-errors');
+require('winston-mongodb')
+const winston = require('winston');
+module.exports = function () {
+    // winston.exceptions.handle(
+    //     new winston.transports.Console({ colorize: true, prettyprint: true}),
+    //     new winston.transports.File({ filename: 'uncaughtExceptions.log'})
+    // )
+
+    //un Caught Exception
+    process.on('uncaughtException', (ex) => {
+        console.log(ex);
+        winston.error(ex.message, ex);
+        process.exit(1);
+    })
+    //unhandle Promise rejection
+    process.on('unhandledRejection', (ex) => {
+        console.log(ex);
+        winston.error(ex.message, ex);
+        process.exit(1);
+    })
+
+    //log error and exception in logFile.log
+    const transportsFile = new winston.transports.File({
+        filename: 'logFile.log'
+    });
+    winston.add(transportsFile)
+
+    //log error in mongo db
+    const transportsDb =
+        new (winston.transports.MongoDB)({
+            db: config.get('db'),
+            level: 'error'
+        })
+    winston.add(transportsDb)
+
+}
