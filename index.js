c2bd7f6cdf711d5cedaf93e1567f3d4cbacaa92a
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-11 12:09:36 +0430

    moved config code and dependenceis in config module

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const config = require('config');
 const app = express();
-require('./startup/loggin')
-require('./startup/routes')(app)
+require('./startup/loggin');
+require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
 
-if (!config.get('jwtPrivateKey')) {
-    console.log('fatal error: jwtPrivatekey not define !');
-    process.exit(1)
-}
 // connecting app settings
 const port = process.env.port || 3000;
 app.listen(port, () => winston.info(`app is listennig on port ${port} . . .`));
