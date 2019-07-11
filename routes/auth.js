a8e7d807e6b4b5a36f0d097c68bbde30977a5d33
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-11 11:06:50 +0430

    create register route

--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,26 @@ const router = express.Router();
 const joi = require('joi');
 const bcrypt = require('bcrypt');
 const lodash = require('lodash');
-const { User } = require('../models/user')
-
+const { User, validate } = require('../models/user')
+const asyncMiddleware = require('../middleware/asyncmiddleware')
 const authService = require('../service/auth.service')
+
+router.post('/register', asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const result =await authService.register(req.body);
+    res.status(400).send(result)
+}))
+
 //log in user route
-router.post('/login', async (req, res) => {
+router.post('/login', asyncMiddleware(async (req, res) => {
     //check validation
     const { error } = authService.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     const result = authService.userLogin(req.body);
     if (!result.ok) return res.status(400).send(result);
     res.header('x-auth-token', result.token).send(`welcome ${result.user.name} !`);
-})
+}))
 
 
 //create validate function for login
