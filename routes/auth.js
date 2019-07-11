cd4f77d60c2e073a78909c3db120bbdea344ae98
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-11 11:14:47 +0430

    bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const joi = require('joi');
-const bcrypt = require('bcrypt');
-const lodash = require('lodash');
-const { User, validate } = require('../models/user')
+const { validate } = require('../models/user')
 const asyncMiddleware = require('../middleware/asyncmiddleware')
 const authService = require('../service/auth.service')
 
