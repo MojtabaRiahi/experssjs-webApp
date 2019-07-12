b7e1c1817463690a4df30988794d15429226354d
mojtaba_r21 <mojtabariahi.developer@gmail.com>
2019-07-12 20:09:19 +0430

    write test for post model

---
+++ tests/unit/models/post.test.js
@@ -0,0 +1,8 @@
+const {validate} =require("../../../models/post")
+describe('validate post model',()=>{
+    it("should return validate data",()=>{
+        const post={title:"new post",description:"decription for new post",imageUrl:"postimage.png",tags:["tag1","tag2","tag3"]}
+        const result=validate(post);
+        expect(result.error).toBeNull()
+    })
+})
